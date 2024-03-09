9d25d99fd20a475695a5f57f541e102b6e0d8727
AAVinogradov <aavinogradov@bk.ru>
2024-03-09 16:14:03 +0300

    создание директории дз

---
+++ HomeWork/src/HW2_05_1.java
@@ -0,0 +1,20 @@
+public class HW2_05_1 {
+    public static void main(String[] args) {
+        int paperReserve = 600000;
+        int inkReserve = 600000;
+        int coverReserve = 2000;
+
+        double printRollerMinTemp = 80.5;
+        double printRollerMaxTemp = 120.75;
+
+        int pagesInBook = 530;
+        int booksCount = 2000;
+
+        boolean coldPrintingMode = false;
+        double printRollerTemp = 90;
+
+
+
+
+    }
+}
