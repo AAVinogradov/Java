00f166c23bcc34615bb1b3d1a8c056dac54e6924
AAVinogradov <aavinogradov@bk.ru>
2024-03-11 22:49:23 +0300

    решение задачи 2.05.1 печатающий станок

--- HomeWork/src/HW2_05_1.java
+++ HomeWork/src/HW2_05_1.java
@@ -1,20 +1,30 @@
 public class HW2_05_1 {
     public static void main(String[] args) {
-        int paperReserve = 600000;
-        int inkReserve = 600000;
-        int coverReserve = 2000;
+        int paperReserve = 600000; // запас бумаги
+        int inkReserve = 600000; // запас чернил
+        int coverReserve = 2000; // запас листов для обложек
 
-        double printRollerMinTemp = 80.5;
-        double printRollerMaxTemp = 120.75;
+        double printRollerMinTemp = 80.5; // нижняя граница температуры
+        double printRollerMaxTemp = 120.75; // верхняя граница температуры
 
-        int pagesInBook = 530;
-        int booksCount = 2000;
+        //Динамические данные
+        int pagesInBook = 530; // задание на печать - количество страниц в книге
+        int booksCount = 2000; // задание на печать - количество книг
+        boolean coldPrintingMode = false;  // режим холодной печати
+        double printRollerTemp = 50; // текущаяя температура ролика
 
-        boolean coldPrintingMode = false;
-        double printRollerTemp = 90;
+        int papersCount = booksCount * pagesInBook / 2;
+        int inkedPaperCount = papersCount + booksCount;
+
+        boolean consumables = paperReserve >= papersCount && inkReserve >= inkedPaperCount && coverReserve >= booksCount;
+        boolean printingTemperature = printRollerTemp > printRollerMinTemp && printRollerTemp < printRollerMaxTemp;
 
 
-
+        if (consumables && printingTemperature || coldPrintingMode) {
+            System.out.println("Печать разрешена");
+        } else {
+            System.out.println("Печать запрещена");
+        }
 
     }
 }
