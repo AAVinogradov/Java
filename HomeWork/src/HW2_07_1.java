5a96b9eb5fe7b007afad8d1003de5fc455170d3a
AAVinogradov <aavinogradov@bk.ru>
2024-03-12 21:48:58 +0300

    решение задачи 2.07.1 тернарный оператор

---
+++ HomeWork/src/HW2_07_1.java
@@ -0,0 +1,14 @@
+public class HW2_07_1 {
+    public static void main(String[] args) {
+        boolean errors = true;
+        String message;
+        if (!errors) {
+            System.out.println("Печать разрешена");
+        } else {
+            System.out.println("Печать запрещена");
+        }
+
+        message = !errors ? "Печать разрешена": "Печать запрещена";
+        System.out.println(message);
+    }
+}
